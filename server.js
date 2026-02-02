58c15ce8ae7ebab17cd0a69b97c5156165ceeabf
furkandn <fdnsmn00@gmail.com>
2026-02-02 23:29:00 +0300

    FIX_V14: Manual HTML file to bypass Vite corruption

--- server.js
+++ server.js
@@ -253,7 +253,7 @@ app.put('/api/users/:id/profile', async (req, res) => {
 
 // Health Check
 app.get('/', (req, res) => {
-    res.send('Chat System Backend is Running (FIX_V13)');
+    res.send('Chat System Backend is Running (FIX_V14)');
 });
 
 // DEBUG: DB Check

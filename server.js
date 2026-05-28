b4203fef350bc1b7b2471f6c7d565543741d07a6
furkandn <fdnsmn00@gmail.com>
2026-05-28 05:11:44 +0300

    Diag: Add timeouts to nodemailer transporter

--- server.js
+++ server.js
@@ -867,14 +867,20 @@ const transporter = nodemailer.createTransport(
             auth: {
                 user: process.env.EMAIL_USER || '',
                 pass: process.env.EMAIL_PASS || ''
-            }
+            },
+            connectionTimeout: 5000,
+            greetingTimeout: 5000,
+            socketTimeout: 5000
           }
         : {
             service: 'gmail',
             auth: {
                 user: process.env.EMAIL_USER || '',
                 pass: process.env.EMAIL_PASS || ''
-            }
+            },
+            connectionTimeout: 5000,
+            greetingTimeout: 5000,
+            socketTimeout: 5000
           }
 );
 
